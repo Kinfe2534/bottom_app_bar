f358b3781adcaecf5ea0da53ee488c225d508648
kinfe mulugeta <kinfe2534@gmail.com>
2021-07-22 11:04:40 +0300

    second commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,7 +13,40 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Container(),
+      body: SafeArea(
+        child: Container(),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.blue.shade200,
+        shape: CircularNotchedRectangle(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.access_alarm),
+              color: Colors.white,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.bookmark_border),
+              color: Colors.white,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.flight),
+              color: Colors.white,
+              onPressed: () {},
+            ),
+            Divider(),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue.shade200,
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
